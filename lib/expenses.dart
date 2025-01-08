ca9b40a027b92a4231ead9108eda56b6c9af0689
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2025-01-08 23:55:14 +0200

    Add: expenses_list file to show dummy data using ListView.builder method for efficient performance

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,24 +1,43 @@
+import 'package:expenses_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expenses_tracker/models/expense.dart';
 
-class Expenses extends StatefulWidget{
-  const Expenses ({super.key});
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
   @override
   State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  // Adding dummy data
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Groceries',
+      amount: 50.0,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'cinema',
+      amount: 20.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('Chart'),
-          Text('Expenses...'),
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
-    
   }
 }
