e6a6a947a7c45a779451b6eb995acfb75efe1648
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2025-02-11 20:32:43 +0200

    Add: property to the list view items to be dismissable

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,13 +3,20 @@ import 'package:expenses_tracker/models/expense.dart';
 import 'package:expenses_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.expenses, required this. onRemoveExpense});
   final List<Expense> expenses;
+  final void Function (Expense expense) onRemoveExpense;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expense:expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        child: ExpenseItem(expense: expenses[index]),
+        onDismissed: (direction){
+          onRemoveExpense(expenses[index]);
+        },
+      ),
     );
   }
 }
