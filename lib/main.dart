00c8586358a0e673c4784107a0549fb960c41f1a
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2025-03-02 08:29:19 +0200

    Make the app more responsive by adjusting the chart and list widgets based on the screen orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 import 'package:flutter/services.dart';
 
 // ColorScheme class provides .fromseed constructor function to generate a shade of colors based on a base color (seed color).
@@ -13,62 +12,61 @@ var kDarkColorTheme = ColorScheme.fromSeed(
 );
 void main() {
   // The following line is to ensure that locking the orientation and running the app work as intended.
-  WidgetsFlutterBinding.ensureInitialized();
+  //WidgetsFlutterBinding.ensureInitialized();
   // Adding then() method because setPreferencesOrientations gives a future
   // making the runApp inside the body of the anonymous function is to make the UI applied once the device orientation is locked.
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((fn) {
-    runApp(
-      MaterialApp(
-        // Apply theming to the entire application
-        // Using the copyWith method to apply the default flutter theme to the application with the ability
-        // to override the default values without having to redefine the entire theme
-        darkTheme: ThemeData.dark().copyWith(
-          colorScheme: kDarkColorTheme,
-          cardTheme: const CardTheme().copyWith(
-            color: kDarkColorTheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kDarkColorTheme.primaryContainer,
-                foregroundColor: kDarkColorTheme.onPrimaryContainer),
+  //SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((fn) {
+  runApp(
+    MaterialApp(
+      // Apply theming to the entire application
+      // Using the copyWith method to apply the default flutter theme to the application with the ability
+      // to override the default values without having to redefine the entire theme
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorTheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorTheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
           ),
         ),
-        theme: ThemeData().copyWith(
-          colorScheme: kColorTheme,
-          // Setting up the default style for the app bar theme
-          appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kColorTheme.onPrimaryContainer,
-              foregroundColor: kColorTheme.primaryFixed),
-          // Setting up the default style for the card theme
-          cardTheme: const CardTheme().copyWith(
-            color: kColorTheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          ),
-          // Setting up the default style for the elevated buttons
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColorTheme.primaryContainer,
-            ),
-          ),
-          // Setting up the default style for the text theme
-          textTheme: ThemeData().textTheme.copyWith(
-                // Setting up the defualt style for the large titles in the app and using (copy with) to just override the selected parts without
-                // redefining the entire object. it depends on how much change you want
-                titleLarge: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: kColorTheme.onSecondaryContainer),
-              ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorTheme.primaryContainer,
+              foregroundColor: kDarkColorTheme.onPrimaryContainer),
         ),
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.system,
-        home: const Expenses(),
       ),
-    );
-  });
+      theme: ThemeData().copyWith(
+        colorScheme: kColorTheme,
+        // Setting up the default style for the app bar theme
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorTheme.onPrimaryContainer,
+            foregroundColor: kColorTheme.primaryFixed),
+        // Setting up the default style for the card theme
+        cardTheme: const CardTheme().copyWith(
+          color: kColorTheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        // Setting up the default style for the elevated buttons
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorTheme.primaryContainer,
+          ),
+        ),
+        // Setting up the default style for the text theme
+        textTheme: ThemeData().textTheme.copyWith(
+              // Setting up the defualt style for the large titles in the app and using (copy with) to just override the selected parts without
+              // redefining the entire object. it depends on how much change you want
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: kColorTheme.onSecondaryContainer),
+            ),
+      ),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      home: const Expenses(),
+    ),
+  );
+  // });
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -3,6 +3,7 @@ import 'package:expenses_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses_tracker/models/expense.dart';
 import 'package:expenses_tracker/widgets/chart/chart.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
@@ -34,6 +35,7 @@ class _ExpensesState extends State<Expenses> {
       builder: (ctx) => NewExpense(onAddExpense: _addNewExpense),
     );
   }
+
   //Function to add new expense item
   void _addNewExpense(Expense expense) {
     setState(() {
@@ -73,6 +75,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     // Declare a widget that represents the main content
     Widget mainContent = const Center(
       child: Text(
@@ -81,17 +84,36 @@ class _ExpensesState extends State<Expenses> {
     );
     if (_registeredExpenses.isNotEmpty) {
       setState(() {
-        mainContent = Column(
-          children: [
-            Chart(expenses: _registeredExpenses),
-            Expanded(
-              child: ExpensesList(
-                expenses: _registeredExpenses,
-                onRemoveExpense: _removeExpense,
-              ),
-            ),
-          ],
-        );
+        mainContent = width < 600
+            ? Column(
+                children: [
+                  Chart(expenses: _registeredExpenses),
+                  Expanded(
+                    child: ExpensesList(
+                      expenses: _registeredExpenses,
+                      onRemoveExpense: _removeExpense,
+                    ),
+                  ),
+                ],
+              )
+            :
+            // Chart widget gets infinity width as it gets width as much it can get
+            Row(
+                children: [
+                  // Expanded constraints in the child (i.e, Chart) to only take as much width as available in the Row after sizing the other Row children
+                  Expanded(
+                    child:
+                        // Chart widget gets infinity width as it gets width as much it can get
+                        Chart(expenses: _registeredExpenses),
+                  ),
+                  Expanded(
+                    child: ExpensesList(
+                      expenses: _registeredExpenses,
+                      onRemoveExpense: _removeExpense,
+                    ),
+                  ),
+                ],
+              );
       });
     }
     return Scaffold(
